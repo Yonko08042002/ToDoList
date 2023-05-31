a381a393161371100d466c2b905500f186566002
Yonko08042002 <tin08042002@gmail.com>
2023-05-31 08:12:33 +0700

    commit function saveData

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const inputBox = document.getElementById("input-box");
 const listcontainer = document.getElementById("list-container");
+const filterTodo = document.getElementById("filter-todo");
 
 
 let addTask =()=>{
@@ -12,7 +13,47 @@ let addTask =()=>{
         listcontainer.appendChild(li);
         let span =document.createElement('span');
         span.innerHTML="\u00d7";
-        li.appendChild(span);
+        li.appendChild(span);       
     }
     inputBox.value = '';
+    saveData();
 }
+
+listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName ==='LI'){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+let saveData=()=>{
+    localStorage.setItem("data", listcontainer.innerHTML);
+    
+}
+let showTask=()=>{
+    localStorage.innerHTML=localStorage.getItem("data");
+}
+showTask();
+
+
+// function all(){
+//     const todos = listcontainer.childNodes;
+//     todos.forEach(function(todo){
+//         todo.style.display="flex";
+//     })
+// }
+
+// function completed(){
+//     const todos = listcontainer.childNodes;
+//     todos.forEach(function(todo){
+//         if(todo.classList.toggle("checked")){
+//         todo.style.display="flex";
+//         }  else{
+//             todo.style.display="none";
+//         } 
+//     })
+// }
